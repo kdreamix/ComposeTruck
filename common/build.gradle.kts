6f23095dd2ba2067e406ba388baa8bdc693b00cb
kit mak <kmak@fitbit.com>
2021-08-23 00:35:03 +0800

    Add deps

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.compose.compose
 
 plugins {
     kotlin("multiplatform")
+    kotlin("plugin.serialization") version "1.5.20"
     id("org.jetbrains.compose") version "0.3.1"
     id("com.android.library")
     id("kotlin-android-extensions")
@@ -9,6 +10,8 @@ plugins {
 
 group = "me.kit"
 version = "1.0"
+
+
 
 repositories {
     google()
@@ -22,11 +25,21 @@ kotlin {
         }
     }
     sourceSets {
+        val ktor_version = "1.6.2"
+        val logack_version = "1.2.5"
+        val kodein = "7.5.0"
         val commonMain by getting {
             dependencies {
                 api(compose.runtime)
                 api(compose.foundation)
                 api(compose.material)
+                api("io.ktor:ktor-client-core:$ktor_version")
+                api("io.ktor:ktor-client-cio:$ktor_version")
+                api("io.ktor:ktor-client-serialization:$ktor_version")
+                api("ch.qos.logback:logback-classic:$logack_version")
+                api("io.ktor:ktor-client-logging:$ktor_version")
+                api("org.kodein.di:kodein-di:$kodein")
+                api("org.kodein.di:kodein-di-framework-compose:$kodein")
             }
         }
         val commonTest by getting
@@ -34,6 +47,7 @@ kotlin {
             dependencies {
                 api("androidx.appcompat:appcompat:1.2.0")
                 api("androidx.core:core-ktx:1.3.1")
+                implementation("io.ktor:ktor-client-okhttp:$ktor_version")
             }
         }
         val androidTest by getting {
